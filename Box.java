e6c0627bbe0318b4808ecc3e2efa50bc9e72403e
Rocco Barbini <roccobarbi@gmail.com>
2016-09-25 19:01:09 +0200

    Defined the Box constructor

--- Box.java
+++ Box.java
@@ -17,6 +17,7 @@ public class Box {
 	private Cell[] boxCells; // Cells in the box, to be created by the constructor
 	private int freeSpace; // Number of cells still to be filled
 	private Board father; // Board to which the box belongs
+	boolean isActive;
 	
 	public int[] getAvailableNumbers(){
 		return availableNumbers;
@@ -32,5 +33,23 @@ public class Box {
 	}
 	public Board getFather(){
 		return father;
+	}
+	
+	public Box(){
+		isActive = false; // Can't be created empty
+	}
+	public Box(Board theFather, int rowOne, int colOne, int[] cellValues){
+		if((cellValues.length != 9) || (rowOne < 1) || (rowOne > 7) || (colOne < 1) || (colOne > 7))
+			isActive = false;
+		else{
+			father = theFather;
+			startRow = rowOne;
+			startColumn = colOne;
+			boxCells = new Cell[9];
+			for(int i = 0; i < cellValues.length; i++){
+				// procedo per riga e colonna
+				boxCells[i] = new Cell(rowOne + (i / 3), colOne + (i % 3), cellValues[i], this);
+			}
+		}
 	}
 }
