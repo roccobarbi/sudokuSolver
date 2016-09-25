b8475c4d981befdbf17e0d5045545a68ce13d97f
Rocco Barbini <roccobarbi@gmail.com>
2016-09-25 22:07:29 +0200

    Move list implemented
    Added private methods to Board class to push and poll moves from the
    queue

--- Board.java
+++ Board.java
@@ -12,4 +12,29 @@ package sudokuSolver;
 public class Board {
 	private Box[] boardBox;
 	private Move[] moveQueue;
+	
+	private void pushMove(int row, int col, int value){
+		Move theMove = new Move(row, col, value);
+		if(theMove.getIsValid()){
+			int theLength = moveQueue.length + 1;
+			Move[] theQueue = new Move[theLength];
+			for(int i = 0; i < theLength - 1; i++)
+				theQueue[i] = moveQueue[i];
+			theQueue[theLength - 1] = theMove;
+			moveQueue = theQueue;
+		}
+	}
+	private Move pollMove(){
+		int theLength = moveQueue.length;
+		if(theLength == 0)
+			return null;
+		Move theMove = moveQueue[0];
+		theLength -= 1;
+		Move[] theQueue = new Move[theLength];
+		if(theLength > 0)
+			for(int i = 0; i < theLength; i++)
+				theQueue[i] = moveQueue[i + 1];
+		moveQueue = theQueue;
+		return theMove;
+	}
 }
